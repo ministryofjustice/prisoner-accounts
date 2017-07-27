e4c280f31d50ce35ce55550c34ba5531938df53f
mikejackson <michael.jackson@digital.justice.gov.uk>
2017-07-27 11:41:52 +0100

    Initial commit

---
+++ src/main/java/uk/gov/justice/digital/prisoneraccounts/PrisonerAccounts.java
@@ -0,0 +1,9 @@
+package uk.gov.justice.digital.prisoneraccounts;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class PrisonerAccounts {
+}
---
+++ src/main/java/uk/gov/justice/digital/prisoneraccounts/service/AccountService.java
@@ -0,0 +1,7 @@
+package uk.gov.justice.digital.prisoneraccounts.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+}
