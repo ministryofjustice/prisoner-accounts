eaa88f78315f859f76bb877f5af8987f8c0c7b33
Matt Smith <mattonfoot@gmail.com>
2017-11-13 13:16:00 +0000

    invalid image value

--- scripts/generateDockerrun.sh
+++ scripts/generateDockerrun.sh
@@ -6,14 +6,11 @@ cd $PWD
 
 cat <<- _EOF_ > Dockerrun.aws.json
 {
-  "AWSEBDockerrunVersion": "2",
+  "AWSEBDockerrunVersion": 2,
   "containerDefinitions": [
     {
       "name": "prisoner-accounts-web",
-      "image": {
-        "name": "mojdigitalstudio/prisoner-accounts-web:${BUILD_VERSION}",
-        "update": "true"
-      },
+      "image": "mojdigitalstudio/prisoner-accounts-web:${BUILD_VERSION}",
       "portMappings": [
         {
           "hostPort": 3000,
@@ -23,10 +20,7 @@ cat <<- _EOF_ > Dockerrun.aws.json
     },
     {
       "name": "prisoner-accounts-service",
-      "image": {
-        "name": "mojdigitalstudio/prisoner-accounts-service:${BUILD_VERSION}",
-        "update": "true"
-      },
+      "image": "mojdigitalstudio/prisoner-accounts-service:${BUILD_VERSION}",
       "portMappings": [
         {
           "hostPort": 8080,
