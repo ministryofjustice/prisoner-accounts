12de610494b61e72882d1c8b67c550dc28e5701d
Matt Smith <mattonfoot@gmail.com>
2017-11-13 13:23:38 +0000

    additional environment variables in dockerrun

--- scripts/generateDockerrun.sh
+++ scripts/generateDockerrun.sh
@@ -11,20 +11,49 @@ cat <<- _EOF_ > Dockerrun.aws.json
     {
       "name": "prisoner-accounts-web",
       "image": "mojdigitalstudio/prisoner-accounts-web:${BUILD_VERSION}",
+      "environment": [
+        {
+          "name": "ACCOUNT_SERVICE_HOST",
+          "value": "prisoner-accounts-service"
+        },
+        {
+          "name": "ACCOUNT_SERVICE_PORT",
+          "value": "8080"
+        }
+      ],
+      "essential": true,
+      "memory": 128,
       "portMappings": [
         {
-          "hostPort": 3000,
+          "hostPort": 80,
           "containerPort": 3000
+        }
+      ],
+      "links": [
+        "prisoner-accounts-service"
+      ],
+      "mountPoints": [
+        {
+          "sourceVolume": "awseb-logs-prisoner-accounts-web",
+          "containerPath": "/var/log/prisoner-accounts-web"
         }
       ]
     },
     {
       "name": "prisoner-accounts-service",
       "image": "mojdigitalstudio/prisoner-accounts-service:${BUILD_VERSION}",
+      "essential": true,
+      "memory": 128,
       "portMappings": [
         {
           "hostPort": 8080,
           "containerPort": 8080
+        }
+      ],
+      "mountPoints": [
+        {
+          "sourceVolume": "awseb-logs-prisoner-accounts-service",
+          "containerPath": "/var/log/prisoner-accounts-service"
         }
       ]
     }
